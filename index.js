573b6ee4b2edb045554e44ede2b6327388230ef5
JuanVasquezJaramillo <juapablovasquez2003@gmail.com>
2023-07-27 12:54:38 -0500

    ultima

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 5000;
+require('dotenv').config();
+//const PORT = 5000;
 conn.sync({ force: false}).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
 }) 
 }).catch(error => console.error(error))
